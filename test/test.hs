d3de7c3d80d5336c5757c5ac94f01e586093505a
Pierre Abbat <phma@bezitopo.org>
2022-02-14 16:47:49 +0000

    Add more tests of split343

--- test/test.hs
+++ test/test.hs
@@ -21,8 +21,20 @@ qcProps = testGroup "(checked by QuickCheck)"
   ]
 
 unitTests = testGroup "Unit tests"
-  [ testCase "split343 524288" $
+  [ testCase "split343 0" $
+      (split343 0) `compare` [] @?= EQ,
+    testCase "split343 1" $
+      (split343 1) `compare` [1] @?= EQ,
+    testCase "split343 342" $
+      (split343 342) `compare` [342] @?= EQ,
+    testCase "split343 343" $
+      (split343 343) `compare` [0,1] @?= EQ,
+    testCase "split343 524288" $
       (split343 524288) `compare` [184,156,4] @?= EQ,
     testCase "split343 531441" $
-      (split343 531441) `compare` [134,177,4] @?= EQ
+      (split343 531441) `compare` [134,177,4] @?= EQ,
+    testCase "split343 129140163" $
+      (split343 129140163) `compare` [320,230,68,3] @?= EQ,
+    testCase "split343 134217728" $
+      (split343 134217728) `compare` [113,285,111,3] @?= EQ
   ]
